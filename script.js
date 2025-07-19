f8080142f82e5cab3476756cfda7921a4cb4530b
NourhanHosnyy <nourhanhosny87@gmail.com>
2025-07-19 17:13:51 +0300

    Add calculator repository

---
+++ script.js
@@ -0,0 +1,23 @@
+const display= document.getElementById("display");
+
+function appendToDisplay(input)
+{
+   display.value+=input;
+}
+function result()
+{
+    try{
+        display.value=eval(display.value);
+    }catch(error){
+        display.value="Error"
+    }
+   
+}
+function clearDisplay()
+{
+   display.value="";
+}
+
+function deleteLastChar() {
+    display.value = display.value.slice(0, -1);
+ }
